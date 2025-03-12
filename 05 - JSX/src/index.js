6179283ec2ef734d8b8604928968f31698cfa59d
GersonESS <gebhsantos@gmail.com>
2025-03-12 14:16:13 -0300

    t

---
+++ 05 - JSX/src/index.js
@@ -0,0 +1,8 @@
+// Importações
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Estrutura do render
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<h1>Meu primeiro render</h1>);
